28cf175e5a4682be8fa236c90aac8fcaae7b8042
RAUX Blandine <braux@fanette.pedago.ipb.fr>
2017-11-09 10:39:51 +0100

    Fonctions aléatoires

--- main.f90
+++ main.f90
@@ -1,11 +1,21 @@
 Program Main
 
-  Use Data
+!  Use Data
   Use functions
-  Use Evolution_Energie
+!  Use Evolution_Energie
 
   implicit none
 
-  Call ReadData()
+  Integer, parameter :: N = 10
+  Real, dimension(N) :: Energies, Vecteur
+  Real :: random
+
+!  Call ReadData()
+
+  Energies = NRJ_Aleatoires(N)
+  write(6,*) Energies
+
+  Vecteur = Vect_Aleatoire(N)
+  write(6,*) Vecteur
 
 End Program Main
