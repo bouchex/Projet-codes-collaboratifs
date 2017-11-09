02b63d1f25acbdc78720653301ad8b8b957ed029
bouchex <bouchex.sylvain@gmail.com>
2017-11-09 11:19:40 +0100

    Ajout Histogramme

---
+++ Histogramme.f90
@@ -0,0 +1,15 @@
+module Histogramme
+
+  implicit none
+
+contains
+
+  Function Histogramme(Tab, inf, sup)
+
+    Real, dimension(1:100) :: Histogramme
+    Real, dimension(:), intent(in) ::Tab
+    Real, intent(in) :: inf, sup
+    integer :: i
+
+    do i=1,
+End module Histogramme
