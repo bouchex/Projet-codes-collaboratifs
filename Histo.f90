8ea2d44f8bbb4e06c0172f3bfd82052df7b05ecf
RAUX Blandine <braux@comminges.pedago.ipb.fr>
2017-12-12 14:15:17 +0100

    Histo.f90

---
+++ Histo.f90
@@ -0,0 +1,46 @@
+module Histo
+
+  implicit none
+
+contains
+
+Real Function sortie(Tab, taille_histo, inf, sup)
+
+    Real, dimension(1:taille_histo) :: Histogramme
+    Real, dimension(:), intent(in) ::Tab
+    Real, intent(in) :: inf, sup
+    Integer, intent(in) :: taille_histo
+    Real, dimension(1:taille_histo) :: Bornes_histo
+    Real :: ecart
+    integer :: i, j
+
+    ecart = (sup-inf)/(taille_histo*1.)
+
+    do i=1,taille_histo
+       Histogramme(i) = 0
+       Bornes_histo(i) = inf + (i-1) * ecart
+    end do
+
+    do i=1, size(Tab)
+       if (Tab(i) > Bornes_histo(taille_histo)) then
+          Histogramme(taille_histo) = Histogramme(taille_histo) + 1
+       else
+          do j=2, taille_histo
+             if (Tab(i) < Bornes_histo(j)) then
+                Histogramme(j-1) = Histogramme(j-1) + 1
+                exit
+            end if
+          end do
+       end if
+    end do
+    Open(1,file='Histogramme.dat')
+    do i = 1,size(Histogramme)
+        write(1,*) Histogramme(i)
+    end do
+    Close(1)
+
+    sortie = 1.
+  end Function sortie
+
+
+End module Histo
